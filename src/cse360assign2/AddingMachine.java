58b5217226507e043d5836b9d6dd19450e2e743f
Brucebb97 <56213540+Brucebb97@users.noreply.github.com>
2019-10-09 19:56:40 -0700

    Second Version
    1.Add the parameter to the total variable
    2.Subtract the parameter from the total variable
    3.The getTotal method should return the current total

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -1,30 +1,58 @@
 package cse360assign2;
+/*Liangbin Gao*/
+/*Studen ID : 1213031025*/
+
 
 public class AddingMachine {
 
-	private int total;
-	
-	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
-	}
-	
-	public int getTotal () {
-		return 0;
-	}
-	
-	public void add (int value) {
-		
-	}
-	
-	public void subtract (int value) {
-		
-	}
-		
-	public String toString () {
-		return "";
-	}
+    private int total = 0;
+    private String processString = "0";
 
-	public void clear() {
-	
-	}
+    public AddingMachine() {
+
+    }
+
+    public int getTotal() {
+        return this.total;
+    }
+
+    public void add(int value) {
+        this.total += value;
+        this.processString += "+" + value;
+    }
+
+    public void subtract(int value) {
+        this.total -= value;
+        this.processString += "-" + value;
+    }
+
+    public String toString() {
+        return this.processString;
+    }
+
+    public void clear() {
+        this.total = 0;
+        this.processString = "0";
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getProcessString() {
+        return processString;
+    }
+
+    public void setProcessString(String processString) {
+        this.processString = processString;
+    }
+
+    public static void main(String[] args){
+        AddingMachine myCalculator = new AddingMachine();
+        myCalculator.add (4);
+        myCalculator.subtract (2);
+        myCalculator.add(5);
+        System.out.println(myCalculator);
+        System.out.println(myCalculator.getTotal());
+    }
 }
